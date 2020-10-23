ed283a876eaf82c9eba85e90ef228c16cb8fbf89
Junichi Tanaka <junichi200123@gmail.com>
2020-10-24 02:27:23 +0900

    load_saveの行数を22行に抑えた

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,41 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 00:15:26 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/24 02:01:17 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/24 02:26:56 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+static int load_save(char **line, char **save)
+{
+	char *tmp;
+	
+	if (!*save)
+		return (1);
+	// *save 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *save に入れて return
+	if (ft_strchr(*save, '\n'))
+	{
+		tmp = *line;
+		*line = ft_substr(*save, 0, ft_strchr(*save, '\n') - *save);
+		free(tmp);
+		if (!(*line))
+			return (-1);
+		tmp = *save;
+		*save = ft_substr(ft_strchr(*save, '\n') + 1, 0, ft_strlen(ft_strchr(*save, '\n')));
+		free(tmp);
+		if (!(*save))
+			return (-1);
+		return (1);
+	}
+	// *save 内に改行が入っていない
+	tmp = *line;
+	*line = *save;
+	*save = NULL;
+	free(tmp);
+	return (1);
+}
 
 int get_next_line(int fd, char **line)
 {
@@ -24,38 +53,10 @@ int get_next_line(int fd, char **line)
 	buf = malloc(BUFFER_SIZE + 1);  // ヒープ領域に確保する必要ある?
 	*line = malloc(1);
 	*line[0] = '\0';
-	tmp = NULL;
-	old_line = NULL;
-	// *save 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *save に入れて return
-	if (save && ft_strchr(save, '\n'))
-	{
-		old_line = *line;
-		if (!(*line = ft_substr(save, 0, ft_strchr(save, '\n') - save)))
-		{
-			free(old_line);
-			free(buf);
-			return (-1);
-		}
-		free(old_line);
-		tmp = save;
-		if (!(save = ft_substr(ft_strchr(save, '\n') + 1, 0, ft_strlen(ft_strchr(save, '\n')))))
-		{
-			free(tmp);
-			free(buf);
-			return (-1);
-		}
-		free(tmp);
-		free(buf);
-		return (1);
-	}
-	// save 内に改行が入っていない
-	else if (save && !ft_strchr(save, '\n'))
-	{
-		old_line = *line;
-		*line = save;
-		save = NULL;
-		free(old_line);
-	}
+
+	// 前回保存した *save から *line に読み込む
+	if (save)
+		load_save(line, &save);
 	
 	// 改行が出現するまで読み込んで繋げる.
 	// 改行が出現したらそれまでの文字列を*lineに入れて, それ以降の文字列を*saveに入れる
