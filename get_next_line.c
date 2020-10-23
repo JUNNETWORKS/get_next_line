2f487a68f52d554f8595a1a0012833ea2e963638
Junichi Tanaka <junichi200123@gmail.com>
2020-10-24 02:01:52 +0900

    change name from save_str to save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 00:15:26 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/24 01:56:20 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/24 02:01:17 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,26 @@ int get_next_line(int fd, char **line)
 	char *buf;
 	char *tmp;
 	char *old_line;  // free() 用
-	static char *next_str;  // 次使う文字列
+	static char *save;  // 次使う文字列
 
 	buf = malloc(BUFFER_SIZE + 1);  // ヒープ領域に確保する必要ある?
 	*line = malloc(1);
 	*line[0] = '\0';
 	tmp = NULL;
 	old_line = NULL;
-	// *next_str 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *next_str に入れて return
-	if (next_str && ft_strchr(next_str, '\n'))
+	// *save 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *save に入れて return
+	if (save && ft_strchr(save, '\n'))
 	{
 		old_line = *line;
-		if (!(*line = ft_substr(next_str, 0, ft_strchr(next_str, '\n') - next_str)))
+		if (!(*line = ft_substr(save, 0, ft_strchr(save, '\n') - save)))
 		{
 			free(old_line);
 			free(buf);
 			return (-1);
 		}
 		free(old_line);
-		tmp = next_str;
-		if (!(next_str = ft_substr(ft_strchr(next_str, '\n') + 1, 0, ft_strlen(ft_strchr(next_str, '\n')))))
+		tmp = save;
+		if (!(save = ft_substr(ft_strchr(save, '\n') + 1, 0, ft_strlen(ft_strchr(save, '\n')))))
 		{
 			free(tmp);
 			free(buf);
@@ -48,22 +48,22 @@ int get_next_line(int fd, char **line)
 		free(buf);
 		return (1);
 	}
-	// next_str 内に改行が入っていない
-	else if (next_str && !ft_strchr(next_str, '\n'))
+	// save 内に改行が入っていない
+	else if (save && !ft_strchr(save, '\n'))
 	{
 		old_line = *line;
-		*line = next_str;
-		next_str = NULL;
+		*line = save;
+		save = NULL;
 		free(old_line);
 	}
 	
 	// 改行が出現するまで読み込んで繋げる.
-	// 改行が出現したらそれまでの文字列を*lineに入れて, それ以降の文字列を*next_strに入れる
+	// 改行が出現したらそれまでの文字列を*lineに入れて, それ以降の文字列を*saveに入れる
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[read_size] = '\0';
 		// if 改行が来たらそれまでの文字列は *line に入れて,
-		//    それ以降の文字列は *next_str に入れる
+		//    それ以降の文字列は *save に入れる
 		if (ft_strchr(buf, '\n'))
 		{
 			if (!(tmp = ft_substr(buf, 0, ft_strchr(buf, '\n') - buf)))
@@ -80,7 +80,7 @@ int get_next_line(int fd, char **line)
 			}
 			free(old_line);
 			free(tmp);
-			if (!(next_str = ft_substr(ft_strchr(buf, '\n') + 1, 0, ft_strlen(ft_strchr(buf, '\n')))))
+			if (!(save = ft_substr(ft_strchr(buf, '\n') + 1, 0, ft_strlen(ft_strchr(buf, '\n')))))
 			{
 				free(buf);
 				return (-1);
