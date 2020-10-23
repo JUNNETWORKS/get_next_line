2e8b45394d351f16d184ee0fa754624351df19a5
Junichi Tanaka <junichi200123@gmail.com>
2020-10-23 22:29:15 +0900

    メモリリークなしで動く

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 00:15:26 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/23 22:19:59 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/23 22:28:56 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int get_next_line(int fd, char **line)
 	int status;  // return status
 	ssize_t read_size;
 	char *buf;
-	char *tmp;  // 今回出力する文字列
-	char *tmp_line;
+	char *previous_str;
+	char *tmp;
+	char *tmp_line;  // free() 用
 	static char *next_str;  // 次使う文字列
 
 	printf("\nnext_str: %s \t %p\n", next_str, next_str);
@@ -31,29 +32,37 @@ int get_next_line(int fd, char **line)
 	buf = malloc(BUFFER_SIZE + 1);
 	*line = malloc(1);
 	*line[0] = '\0';
+	previous_str = NULL;
 	tmp = NULL;
+	tmp_line = NULL;
 	// *next_str に文字列が入っていたら(next_str[0] != '\0')それを *tmp に入れて *next_str を空にする
 	if (next_str)
 	{
 		printf("\n*next_str に文字列が入っていたらそれを *tmp に入れて *next_str を空にする\n");
-		tmp = ft_strdup(next_str);
+		previous_str = ft_strdup(next_str);
 		free(next_str);
 		next_str = NULL;
 	}
 	
-	// *tmp 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *next_str に入れて return
-	if (tmp && ft_strchr(tmp, '\n'))
+	// *previous_str 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *next_str に入れて return
+	if (previous_str && ft_strchr(previous_str, '\n'))
 	{
-		printf("\n*tmp 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *next_str に入れて return\n");
+		printf("\n*previous_str 内に改行が入っていたら改行までを *line にコピーして, 改行以降を *next_str に入れて return\n");
 		tmp_line = *line;
-		*line = ft_substr(tmp, 0, ft_strchr(tmp, '\n') - tmp);
+		*line = ft_substr(previous_str, 0, ft_strchr(previous_str, '\n') - previous_str);
 		free(tmp_line);
-		next_str = ft_substr(ft_strchr(tmp, '\n') + 1, 0, ft_strlen(ft_strchr(tmp, '\n')));
-		free(tmp);
+		next_str = ft_substr(ft_strchr(previous_str, '\n') + 1, 0, ft_strlen(ft_strchr(previous_str, '\n')));
+		free(previous_str);
 		free(buf);
 		return (1);
 	}
-	free(tmp);
+	// previous_str 内に改行が入っていない
+	else if (previous_str && !ft_strchr(previous_str, '\n'))
+	{
+		tmp_line = *line;
+		*line = previous_str;
+		free(tmp_line);
+	}
 
 	// BUFFER_SIZE 読み込む
 	read_size = read(fd, buf, BUFFER_SIZE);
