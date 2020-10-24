0f24077cf796920e470bf3debb2d5c8e86738fc0
Junichi Tanaka <junichi200123@gmail.com>
2020-10-25 02:33:17 +0900

    bonusを追加

---
+++ get_next_line_bonus.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 02:22:38 by jtanaka           #+#    #+#             */
+/*   Updated: 2020/10/25 02:22:39 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+static int	join_save_next_str(char **line, char **next_str)
+{
+	char	*tmp;
+
+	if (ft_strchr(*next_str, '\n'))
+	{
+		tmp = *line;
+		*line = ft_substr(*next_str, 0, ft_strchr(*next_str, '\n') - *next_str);
+		free(tmp);
+		if (!(*line))
+			return (ERROR);
+		tmp = *next_str;
+		*next_str = ft_substr(ft_strchr(*next_str, '\n') + 1, 0,
+								ft_strlen(ft_strchr(*next_str, '\n')));
+		free(tmp);
+		if (!(*next_str))
+			return (ERROR);
+		return (SUCCESS);
+	}
+	else
+	{
+		tmp = *line;
+		*line = *next_str;
+		*next_str = NULL;
+		free(tmp);
+		return (CONTINUE_PROC);
+	}
+}
+
+static int	split_by_newline(char **line, char **next_str, char *buf)
+{
+	char	*old_line;
+	char	*tmp;
+
+	if (!(tmp = ft_substr(buf, 0, ft_strchr(buf, '\n') - buf)))
+		return (ERROR);
+	old_line = *line;
+	if (!(*line = ft_strjoin(*line, tmp)))
+	{
+		free(old_line);
+		free(tmp);
+		return (ERROR);
+	}
+	free(old_line);
+	free(tmp);
+	if (!(*next_str = ft_substr(ft_strchr(buf, '\n') + 1, 0,
+								ft_strlen(ft_strchr(buf, '\n')))))
+		return (ERROR);
+	return (SUCCESS);
+}
+
+static int	join_line_and_buf(char **line, char *buf)
+{
+	char	*tmp;
+
+	tmp = *line;
+	if (!(*line = ft_strjoin(*line, buf)))
+	{
+		free(tmp);
+		return (ERROR);
+	}
+	free(tmp);
+	return (CONTINUE_PROC);
+}
+
+static int	read_process(int fd, char **line, char **next_str)
+{
+	ssize_t		read_size;
+	int			ret;
+	char		*buf;
+
+	ret = CONTINUE_PROC;
+	if (!(buf = malloc(BUFFER_SIZE + 1)))
+		return (ERROR);
+	while (ret == CONTINUE_PROC && (read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[read_size] = '\0';
+		if (ft_strchr(buf, '\n'))
+			ret = split_by_newline(line, next_str, buf);
+		else
+			ret = join_line_and_buf(line, buf);
+	}
+	free(buf);
+	if (ret == CONTINUE_PROC && read_size == 0)
+		ret = END_OF_FILE;
+	else if (ret == CONTINUE_PROC && read_size == -1)
+		ret = ERROR;
+	return (ret);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	int			ret;
+	static char	*next_str[1024];
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (ERROR);
+	*line = malloc(1);
+	*line[0] = '\0';
+	ret = CONTINUE_PROC;
+	if (next_str[fd])
+		ret = join_save_next_str(line, &next_str[fd]);
+	if (ret == CONTINUE_PROC)
+		ret = read_process(fd, line, &next_str[fd]);
+	return (ret);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 02:22:55 by jtanaka           #+#    #+#             */
+/*   Updated: 2020/10/25 02:22:56 by jtanaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	ans;
+
+	ans = 0;
+	while (s[ans])
+		ans++;
+	return (ans);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t			s_len;
+	char			*new_str;
+	size_t			i;
+
+	s_len = ft_strlen(s);
+	if (!(new_str = (char*)malloc(s_len + 1)))
+		return (NULL);
+	i = 0;
+	while (i < s_len)
+	{
+		new_str[i] = s[i];
+		i++;
+	}
+	new_str[s_len] = '\0';
+	return (new_str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*substr;
+	size_t			i;
+	size_t			s_len;
+
+	if (!(substr = malloc(len + 1)))
+		return (NULL);
+	*substr = '\0';
+	if (start >= ft_strlen(s))
+		return (substr);
+	i = 0;
+	s_len = ft_strlen(s);
+	while (i < len && i < s_len)
+	{
+		substr[i] = *(s + start + i);
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char*)s;
+	while (*str || !c)
+	{
+		if (*str == (char)c)
+			return (str);
+		str++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	char			*ans;
+	unsigned int	total_len;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	total_len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = malloc(total_len + 1)))
+		return (NULL);
+	ans = str;
+	while (*s1)
+		*str++ = *s1++;
+	while (*s2)
+		*str++ = *s2++;
+	*str = '\0';
+	return (ans);
+}
