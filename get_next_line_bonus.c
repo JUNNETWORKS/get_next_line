1da1fc951771666bd21639022e52502b46b342f9
Junichi Tanaka <junichi200123@gmail.com>
2020-10-26 03:24:00 +0900

    連結リストによる複数fd対応試作中

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,10 +6,11 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 02:22:38 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/26 02:51:08 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/26 03:23:37 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "get_next_line_bonus.h"
 
 static int	join_save_save(char **line, char **save)
@@ -102,21 +103,66 @@ static int	read_process(int fd, char **line, char **save)
 	return (ret);
 }
 
+t_save_list *new_lst(int fd)
+{
+	t_save_list *new_lst;
+	
+	if (!(new_lst = malloc(sizeof(t_save_list))))
+		return (NULL);
+	new_lst->fd = fd;
+	new_lst->save = NULL;
+	new_lst->next = NULL;
+	return (new_lst);
+}
+
+void	lstadd_back(t_save_list **lst, t_save_list *new)
+{
+	t_save_list *lst_tmp;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	lst_tmp = *lst;
+	while (lst_tmp->next)
+		lst_tmp = lst_tmp->next;
+	lst_tmp->next = new;
+}
+
 int			get_next_line(int fd, char **line)
 {
-	int				ret;
-	static t_list	save_list;		
-	static char		*save[1024];  // fd が1024超えたらエラー出るから修正が必要
+	int					ret;
+	static t_save_list	*save_list;
+	t_save_list			*save_list_tmp;
+	char				*save;  // fd が1024超えたらエラー出るから修正が必要
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0 || !(*line = ft_strdup("")))
 		return (ERROR);
 	// search fd's t_list
+	save_list_tmp = save_list;
+	while (save_list_tmp)
+	{
+		if (save_list_tmp->fd == fd)
+			break;
+		save_list_tmp = save_list_tmp->next;
+	}
 	// If fd's t_list is found, save = save_t_list->save;
 	// else it'll be created and add to save_list;
+	if (!save_list_tmp)
+	{
+		printf("fd: %d save_list is not found\n", fd);
+		if (!(save_list_tmp = new_lst(fd)))
+			return (ERROR);
+		lstadd_back(&save_list, save_list_tmp);
+	}
+	save = save_list_tmp->save;
 	ret = CONTINUE_PROC;
-	if (save[fd])
-		ret = join_save_save(line, &save[fd]);
+	if (save)
+		ret = join_save_save(line, &save);
 	if (ret == CONTINUE_PROC)
-		ret = read_process(fd, line, &save[fd]);
+		ret = read_process(fd, line, &save);
 	return (ret);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 02:22:45 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/26 02:55:01 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/26 03:14:34 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@
 
 typedef struct s_save_list
 {
-  int			fd;
-  char			*save;
-  t_save_list	*next;
+  int			            fd;
+  char			          *save;
+  struct s_save_list  *next;
 } t_save_list;
 
 
