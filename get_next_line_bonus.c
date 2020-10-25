03e2d46bf32c64362486df42b8cf9a973b421d2f
Junichi Tanaka <junichi200123@gmail.com>
2020-10-26 02:52:39 +0900

    next_str を save に名前変更 (in bonus)

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,42 +6,42 @@
 /*   By: jtanaka <jtanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 02:22:38 by jtanaka           #+#    #+#             */
-/*   Updated: 2020/10/25 16:42:06 by jtanaka          ###   ########.fr       */
+/*   Updated: 2020/10/26 02:51:08 by jtanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-static int	join_save_next_str(char **line, char **next_str)
+static int	join_save_save(char **line, char **save)
 {
 	char	*tmp;
 
-	if (ft_strchr(*next_str, '\n'))
+	if (ft_strchr(*save, '\n'))
 	{
 		tmp = *line;
-		*line = ft_substr(*next_str, 0, ft_strchr(*next_str, '\n') - *next_str);
+		*line = ft_substr(*save, 0, ft_strchr(*save, '\n') - *save);
 		free(tmp);
 		if (!(*line))
 			return (ERROR);
-		tmp = *next_str;
-		*next_str = ft_substr(ft_strchr(*next_str, '\n') + 1, 0,
-								ft_strlen(ft_strchr(*next_str, '\n')));
+		tmp = *save;
+		*save = ft_substr(ft_strchr(*save, '\n') + 1, 0,
+								ft_strlen(ft_strchr(*save, '\n')));
 		free(tmp);
-		if (!(*next_str))
+		if (!(*save))
 			return (ERROR);
 		return (SUCCESS);
 	}
 	else
 	{
 		tmp = *line;
-		*line = *next_str;
-		*next_str = NULL;
+		*line = *save;
+		*save = NULL;
 		free(tmp);
 		return (CONTINUE_PROC);
 	}
 }
 
-static int	split_by_newline(char **line, char **next_str, char *buf)
+static int	split_by_newline(char **line, char **save, char *buf)
 {
 	char	*old_line;
 	char	*tmp;
@@ -57,7 +57,7 @@ static int	split_by_newline(char **line, char **next_str, char *buf)
 	}
 	free(old_line);
 	free(tmp);
-	if (!(*next_str = ft_substr(ft_strchr(buf, '\n') + 1, 0,
+	if (!(*save = ft_substr(ft_strchr(buf, '\n') + 1, 0,
 								ft_strlen(ft_strchr(buf, '\n')))))
 		return (ERROR);
 	return (SUCCESS);
@@ -77,7 +77,7 @@ static int	join_line_and_buf(char **line, char *buf)
 	return (CONTINUE_PROC);
 }
 
-static int	read_process(int fd, char **line, char **next_str)
+static int	read_process(int fd, char **line, char **save)
 {
 	ssize_t		read_size;
 	int			ret;
@@ -90,7 +90,7 @@ static int	read_process(int fd, char **line, char **next_str)
 	{
 		buf[read_size] = '\0';
 		if (ft_strchr(buf, '\n'))
-			ret = split_by_newline(line, next_str, buf);
+			ret = split_by_newline(line, save, buf);
 		else
 			ret = join_line_and_buf(line, buf);
 	}
@@ -104,15 +104,19 @@ static int	read_process(int fd, char **line, char **next_str)
 
 int			get_next_line(int fd, char **line)
 {
-	int			ret;
-	static char	*next_str[1024];  // fd が1024超えたらエラー出るから修正が必要
+	int				ret;
+	static t_list	save_list;		
+	static char		*save[1024];  // fd が1024超えたらエラー出るから修正が必要
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0 || !(*line = ft_strdup("")))
 		return (ERROR);
+	// search fd's t_list
+	// If fd's t_list is found, save = save_t_list->save;
+	// else it'll be created and add to save_list;
 	ret = CONTINUE_PROC;
-	if (next_str[fd])
-		ret = join_save_next_str(line, &next_str[fd]);
+	if (save[fd])
+		ret = join_save_save(line, &save[fd]);
 	if (ret == CONTINUE_PROC)
-		ret = read_process(fd, line, &next_str[fd]);
+		ret = read_process(fd, line, &save[fd]);
 	return (ret);
 }
